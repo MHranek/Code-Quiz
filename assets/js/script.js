d7966e5f387e9a488555ec57b41a6fed77948a04
Michael Hranek <mhranek@comcast.net>
2021-12-10 21:36:20 -0500

    Enabled button functionality to swap between the main page, quiz page, and scores page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // declare variables referencing buttons
-var highScores = document.getElementById("high-scores-button");
-var startButton = document.getElementById("start-button");
+var highScoresButton = document.getElementById("high-scores-button");
+var beginQuizButton = document.getElementById("start-button");
 var answerButton1 = document.getElementById("answer1");
 var answerButton2 = document.getElementById("answer2");
 var answerButton3 = document.getElementById("answer3");
@@ -63,10 +63,6 @@ console.log(quiz[3]);
 console.log(quiz[4]);
 
 // hide quiz and high scores on startup
-
-console.log(mainPage);
-console.log(quizPage);
-console.log(scoresPage);
 showMainPage();
 
 // change which section is being shown depending on which button is pressed
@@ -74,20 +70,51 @@ function showMainPage() {
     mainPage.setAttribute("style", "display:inline");
     quizPage.setAttribute("style", "display:none");
     scoresPage.setAttribute("style", "display:none");
-
+    mainPage.dataset.status = "visible";
+    quizPage.dataset.status = "hidden";
+    scoresPage.dataset.status = "hidden";
+    // change the content of the high scores button after showing the main page to display high scores
+    highScoresButton.innerHTML = "High Scores";
+    timer.setAttribute("style", "display:none");
 }
 function showQuizPage() {
     mainPage.setAttribute("style", "display:none");
     quizPage.setAttribute("style", "display:inline");
     scoresPage.setAttribute("style", "display:none");
+    mainPage.dataset.status = "hidden";
+    quizPage.dataset.status = "visible";
+    scoresPage.dataset.status = "hidden";
+    // hides high scores button and shows timer in its place
+    highScoresButton.setAttribute("style", "display:none");
+    timer.setAttribute("style", "display:inline");
 }
 function showScoresPage() {
     mainPage.setAttribute("style", "display:none");
     quizPage.setAttribute("style", "display:none");
     scoresPage.setAttribute("style", "display:inline");
+    mainPage.dataset.status = "hidden";
+    quizPage.dataset.status = "hidden";
+    scoresPage.dataset.status = "visible";
+    // change content of high scores button to show user they can go back to the main page
+    highScoresButton.innerHTML = "Main Page";
+    highScoresButton.setAttribute("style", "display:inline");
+    timer.setAttribute("style", "display:none");
 }
 
 // TODO when corresponding button is clicked change which section is visible
 // make high scores visible
+highScoresButton.addEventListener("click", function() {
+    if (scoresPage.dataset.status === "visible"){
+        showMainPage();
+    } else {
+        showScoresPage();
+    }
+    
+})
 
 // make quiz visible
+beginQuizButton.addEventListener('click', function() {
+    showQuizPage();
+    // TODO start timer for quiz
+
+})
