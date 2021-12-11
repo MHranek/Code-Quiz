66a12040190cc548f2a6327223284116598c6b2c
Michael Hranek <mhranek@comcast.net>
2021-12-10 20:28:43 -0500

    Basic layout complete

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// declare variables referencing elements
+var highScores = document.getElementById("highScores");
+var timer = document.getElementById("timer");
+var answerButton1 = document.getElementById("answer1");
+var answerButton2 = document.getElementById("answer2");
+var answerButton3 = document.getElementById("answer3");
+var answerButton4 = document.getElementById("answer4");
+
+// declare variables referencing the quiz/questions/answers
+var questionSet = {
+    // question: ,
+    // answer1: ,
+    // answer2: ,
+    // answer3: ,
+    // answer4: ,
+    // correctAnswer: ,
+}
+var quiz = [];
